0db760b52521f3da94e1f59893045170a95fcb88
sophie <d00259237@student.dkit.ie>
2024-10-19 12:33:22 +0100

    Implemented getAllUsers method to retrieve users from MySQL database and JDBC connection

--- src/main/java/persistence/UsersDao.java
+++ src/main/java/persistence/UsersDao.java
@@ -7,14 +7,13 @@ import java.util.List;
 public interface UsersDao {
     void addUser(Users user) throws Exception;
 
-
-    Users getUserByUsername(String username) throws Exception;
+//    Users getUserByUsername(String username) throws Exception;
 
     boolean validateUser(String username, String password) throws Exception;
 
     List<Users> getAllUsers() throws Exception;
 
-    void updateUser(Users user) throws Exception;
-
-    void deleteUser(int userId) throws Exception;
+//    void updateUser(Users user) throws Exception;
+//
+//    void deleteUser(int userId) throws Exception;
 }
