840defedcc968f27249a9d9d22cd17beca06fe73
jamzy75 <jamzymup@gmail.com>
2024-10-31 10:44:52 +0000

    Changed method addSong from void to int.

--- src/main/java/persistence/SongDao.java
+++ src/main/java/persistence/SongDao.java
@@ -6,7 +6,7 @@ import musiclibrary.Song;
 public interface SongDao {
 
     // Adds a new song to the database
-    void addSong(Song song) throws Exception;
+    int addSong(Song song) throws Exception;
 
 
     // Deletes a song by its songID
