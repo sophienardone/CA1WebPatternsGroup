a72781daf63989fd9ab1995a3bf3f8f12bedacda
sophie <d00259237@student.dkit.ie>
2024-10-31 13:22:34 +0000

    Added Javadocs

--- src/main/java/persistence/ArtistDaoImpl.java
+++ src/main/java/persistence/ArtistDaoImpl.java
@@ -4,10 +4,7 @@ import musiclibrary.Artist;
 import musiclibrary.User;
 
 import javax.xml.transform.Result;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +12,23 @@ public class ArtistDaoImpl implements ArtistDao {
 
     private final sqlDao sqlDao;
 
+
+    /**
+     * Constructs an ArtistDaoImpl with the specified sqlDao.
+     *
+     * @param sqlDao the sqlDao used to obtain database connections
+     */
     public ArtistDaoImpl(sqlDao sqlDao) {
         this.sqlDao = sqlDao;
     }
 
+
+    /**
+     * Retrieves all artists from the database.
+     *
+     * @return a List of Artist objects representing all artists in the database.
+     * @throws SQLException if a database access error occurs.
+     */
     @Override
     public List<Artist> getAllArtists() throws SQLException {
         List<Artist> artists = new ArrayList<>();
@@ -34,7 +44,7 @@ public class ArtistDaoImpl implements ArtistDao {
                         resultset.getString("name"),
                         resultset.getString("genre"),
                         resultset.getString("country"),
-                        resultset.getTimestamp("date").toLocalDateTime()
+                        resultset.getTimestamp("debutDate").toLocalDateTime()
                 );
                 artists.add(artist);
             }
@@ -43,6 +53,13 @@ public class ArtistDaoImpl implements ArtistDao {
     }
 
 
+    /**
+     * Retrieves an artist by their ID from the database.
+     *
+     * @param artistId the ID of the artist to retrieve.
+     * @return the Artist object representing the artist with the specified ID, or null if not found.
+     * @throws SQLException if a database access error occurs.
+     */
     @Override
     public Artist getArtistById(int artistId) throws SQLException {
         Artist artist = null;
@@ -59,12 +76,45 @@ public class ArtistDaoImpl implements ArtistDao {
                             resultset.getString("name"),
                             resultset.getString("genre"),
                             resultset.getString("country"),
-                            resultset.getTimestamp("date").toLocalDateTime()
+                            resultset.getTimestamp("debutDate").toLocalDateTime()
                     );
                 }
             }
         }
         return artist;
     }
+
+
+    /**
+     * Adds a new artist to the database.
+     *
+     * @param artist the Artist object containing the artist information to be added.
+     * @return the ID of the newly created artist.
+     * @throws SQLException if a database access error occurs.
+     */
+    public int addArtist(Artist artist) throws SQLException {
+        String sql = "INSERT INTO Artists (name, genre, country, debutDate) VALUES (?, ?, ?, ?)";
+        try (Connection connection = sqlDao.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, artist.getName());
+            preparedStatement.setString(2, artist.getGenre());
+            preparedStatement.setString(3, artist.getCountry());
+            preparedStatement.setTimestamp(4, Timestamp.valueOf(artist.getDate()));
+
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Inserting artist failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                } else {
+                    throw new SQLException("Inserting artist failed, no ID obtained.");
+                }
+            }
+        }
+    }
+
 
 }
